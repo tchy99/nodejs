59e35c3f8d21a5310cd3ce867e89514541655457
Carl Ritchy Alexis <63527472+tchy99@users.noreply.github.com>
2021-08-14 00:30:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 require('dotenv').config()
 const express = require('express')
+const http = require("http");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World\n");
+  })
+
+  .listen(process.env.PORT || 8080, "127.0.0.1");
 
 const app = express()
 // load module etudiant
